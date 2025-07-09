a0398eb8627c1a96dcafdb9c6f6ec329a0aee4ea
Yusuf <119488820+y4gg@users.noreply.github.com>
2025-07-09 11:22:46 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
           <div className="flex min-h-screen flex-col items-center justify-center">
             {children}
           </div>
-          <div className="absolute bottom-4 left-4">
+          <div className="absolute bottom-4 right-4">
             <ModeToggle />
           </div>
         </ThemeProvider>
